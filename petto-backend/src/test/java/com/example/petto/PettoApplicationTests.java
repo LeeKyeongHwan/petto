3c74cda39ade21b69c3174daa9a3fb128c591b37
jdy8739 <jdy8739@naver.com>
2021-10-13 18:39:26 +0900

    petto-backend

---
+++ petto-backend/src/test/java/com/example/petto/PettoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
