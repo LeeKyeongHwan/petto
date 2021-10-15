be080eee8883b176c3b61f7a300fc798b1537a38
limcholong <limcho1ong5@naver.com>
2021-10-15 22:05:35 +0900

    홈 화면

--- petto-frontend/src/router/index.js
+++ petto-frontend/src/router/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import PettoHome from '../views/PettoHome.vue'
+import signup from '../views/member/signup.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'PettoHome',
+    component: PettoHome
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: signup
   }
 ]
 
