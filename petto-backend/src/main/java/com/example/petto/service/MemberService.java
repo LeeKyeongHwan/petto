82e4572d2bcae33ddac1c9b3dc9d6f4dba3bbf22
jdy8739 <jdy8739@naver.com>
2021-10-17 14:01:31 +0900

    ..

--- petto-backend/src/main/java/com/example/petto/service/MemberService.java
+++ petto-backend/src/main/java/com/example/petto/service/MemberService.java
@@ -4,19 +4,15 @@ import com.example.petto.controller.request.MemberRequest;
 
 public interface MemberService {
 
-<<<<<<< HEAD
-
-    public boolean login(MemberRequest memberRequest) throws Exception;
-
     public boolean idDupliChk(String id);
 
-    public boolean checkIdValidation(String id) throws Exception;
-
-=======
-    public boolean idDupliChk(String id);
-
->>>>>>> f9ccf9b1e400e744ce2c3d77034cd40f0fecfb17
     public void signup(MemberRequest memberRequest);
 
     public boolean nicknameDupliChk(String nickname);
+
+    public boolean checkValidEmail(String email);
+
+    public String checkValidEmailForPw(String email, String id);
+
+    public void changePassword(MemberRequest memberRequest);
 }
