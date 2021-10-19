770bf18b3f92dbe502879a8ab8435bf1131d4585
limcholong <limcho1ong5@naver.com>
2021-10-19 18:07:56 +0900

    ....

--- petto-frontend/src/views/member/FindIdAndPwPage.vue
+++ petto-frontend/src/views/member/FindIdAndPwPage.vue
@@ -13,9 +13,17 @@
         <div style="margin-top: 60px;">
 
             <div v-if="findId">
-                <p class="normalText">가입시 입력했던 이메일을 입력해주세요.</p>
+                <p class="normalText">생년월일과 가입시 입력했던 이메일을 입력해주세요.</p>
 
-                <input class="mainText" v-model="email" style="width: 200px; color: black;"/>
+                <label>
+                    생년월일(6자)
+                    <input class="mainText" v-model="birthday" style="width: 200px; color: black;"/>
+                </label>
+
+                <label>
+                    이메일
+                    <input class="mainText" v-model="email" style="width: 200px; color: black;"/>
+                </label>
 
                 <v-btn text class="mainText" style="margin-top: 0px;" @click="checkValidEmail">
                     확인
@@ -94,72 +102,57 @@
 
 <script>
 import axios from 'axios'
-
 export default {
     name: 'FindIdAndPwPage',
     data() {
         return {
             findId: false,
             findPw: false,
-
             id: '',
             email: '',
-
+            birthday: '',
             emailValidChk: '',
-
             codeSent: false,
-
             code: '',
             codeMsg: '',
-
             changePw: false,
-
             password: '',
             passwordChk: ''
         }
     },
-
     methods: {
         onFindId() {
             this.findId = true
             this.findPw = false
-
             this.changePw = false
-
             this.id = ''
         },
-
         onFindPw() {
             this.findPw = true
             this.findId = false
-
             this.changePw = false
+            this.birthday = ''
         },
-
         checkValidEmail() {
-
             let formData = new FormData()
-
             let email = this.email
             let id = this.id
-
+            let birthday = this.birthday
             formData.append("email", email)
             formData.append("id", id)
-
+            formData.append("birthday", birthday)
             axios.post('http://localhost:8888/petto/member/checkValidEmail', formData)
             .then((res) => {
                 if(res.data == 'EmailExists') {
                     
                     this.emailValidChk = '이메일로 id를 전송했습니다.'
                 } else if(res.data == 'NotEmailExists') {
-
                     this.emailValidChk = '유효하지 않는 이메일입니다. 다시 확인해주세요 :)'
                 } else {
                     this.$cookies.set("code", res.data, '300s')
                     
                     if(this.$cookies.get("code") != 'unvalid') {
                         this.codeSent = true
-
                     } else {
                         this.codeMsg = '아이디와 이메일이 올바르지않습니다.'
                         this.codeSent = false
@@ -172,10 +165,8 @@ export default {
         },
         checkCode() {
             if(this.$cookies.isKey("code")) {
-
                 if(this.code == this.$cookies.get("code")) {
                     this.changePw = true
-
                     this.findId = false
                     this.findPw = false
                     
@@ -186,18 +177,14 @@ export default {
                 alert('시간이 만료되었습니다.')
             }
         },
-
         changePassword() {
-
             if(this.password != "" && this.password == this.passwordChk) {
                 
                 const id = this.id
                 const password = this.password
-
                 axios.post('http://localhost:8888/petto/member/changePassword', { id, password })
                     .then(() => {
                         alert('비밀번호가 변경되었습니다.')
-
                         this.$router.push({
                             name: 'PettoHome'
                         })
@@ -215,10 +202,8 @@ export default {
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Gowun+Batang:wght@700&display=swap');
-
 .normalText {
     font-family: 'Gowun Batang', serif;
     color: #42b8d4;
 }
-
 </style>
