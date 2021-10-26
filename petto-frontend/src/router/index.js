8b0285ac2125f4a24d479ecc36a69f7145c40e2d
airtrade7 <airtrade7@naver.com>
2021-10-26 20:05:46 +0900

    ...

--- petto-frontend/src/router/index.js
+++ petto-frontend/src/router/index.js
@@ -1,77 +1,84 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-import SignupPage from '../views/member/SignupPage.vue'
-import FindIdAndPwPage from '../views/member/FindIdAndPwPage.vue'
-import PettoHome from '../views/PettoHome.vue'
-import MemberLoginPage from '../views/member/MemberLoginPage.vue'
-import InfoModifyPage from '../views/member/InfoModifyPage.vue'
+import SignupPage from "../views/member/SignupPage.vue";
+import FindIdAndPwPage from "../views/member/FindIdAndPwPage.vue";
+import PettoHome from "../views/PettoHome.vue";
+import MemberLoginPage from "../views/member/MemberLoginPage.vue";
+import InfoModifyPage from "../views/member/InfoModifyPage.vue";
 
-import FacilityListPage from '../views/facility/FacilityListPage.vue'
-import FacilityReadPage from '../views/facility/FacilityReadPage.vue'
+import FacilityListPage from "../views/facility/FacilityListPage.vue";
+import FacilityReadPage from "../views/facility/FacilityReadPage.vue";
 
-import AbandonedAnimal from '../views/abandon/AbandonedAnimal.vue'
+import AbandonedAnimal from "../views/abandon/AbandonedAnimal.vue";
 
-Vue.use(VueRouter)
+import KakaoMap from "../views/map/KakaoMap.vue";
+
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/signupPage',
-    name: 'SignupPage',
-    component: SignupPage
+    path: "/signupPage",
+    name: "SignupPage",
+    component: SignupPage,
   },
   {
-    path: '/findIdAndPwPage',
-    name: 'FindIdAndPwPage',
-    component: FindIdAndPwPage
+    path: "/findIdAndPwPage",
+    name: "FindIdAndPwPage",
+    component: FindIdAndPwPage,
   },
   {
-    path: '/pettohome',
-    name: 'PettoHome',
-    component: PettoHome
+    path: "/pettohome",
+    name: "PettoHome",
+    component: PettoHome,
   },
   {
-    path: '/memberLoginPage',
-    name: 'MemberLoginPage',
-    component: MemberLoginPage
+    path: "/memberLoginPage",
+    name: "MemberLoginPage",
+    component: MemberLoginPage,
   },
   {
-    path: '/infoModify/modify',
-    name: 'InfoModifyPage',
-    component: InfoModifyPage
+    path: "/infoModify/modify",
+    name: "InfoModifyPage",
+    component: InfoModifyPage,
   },
   {
-    path: '/facility/list',
-    name: 'FacilityListPage',
-    component: FacilityListPage
+    path: "/facility/list",
+    name: "FacilityListPage",
+    component: FacilityListPage,
   },
   {
-    path: '/facility/read/:facilityNo', // *뒤로가기가 안됨
-    name: 'FacilityReadPage',
+    path: "/facility/read/:facilityNo", // *뒤로가기가 안됨
+    name: "FacilityReadPage",
     components: {
-      default: FacilityReadPage
+      default: FacilityReadPage,
     },
     props: {
-      default: true
-    }
+      default: true,
+    },
   },
   {
-    path: '/abandonedAnimal',
-    name: 'AbandonedAnimal',
-    component: AbandonedAnimal
-  }
-]
+    path: "/abandonedAnimal",
+    name: "AbandonedAnimal",
+    component: AbandonedAnimal,
+  },
+  {
+    path: "/map",
+    name: "KakaoMap",
+    component: KakaoMap,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
