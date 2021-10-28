0c53988450bb2e1bad40d9f9a052313ce71379c6
airtrade7 <airtrade7@naver.com>
2021-10-28 20:34:10 +0900

    ...

--- petto-frontend/src/views/facility/FacilityListPage.vue
+++ petto-frontend/src/views/facility/FacilityListPage.vue
@@ -1,164 +1,171 @@
 <template>
-    <div align="center" style="padding-top: 60px;">
+  <div align="center" style="padding-top: 60px;">
+    <p class="mainText">전국 보호소 리스트</p>
 
-        <p class="mainText">전국 보호소 리스트</p>
+    <v-container style="margin-top: 60px;">
+      <div style="float: right;">
+        <v-select
+          v-model="selectedAreas"
+          :items="areas"
+          label="지역 선택"
+          multiple
+          style="width: 250px; display: inline-block; margin-right: 40px; font-size: 12px;"
+          persistent-hint
+        >
+        </v-select>
 
-        <v-container style="margin-top: 60px;">
-            
-            <div style="float: right;">
-                <v-select
-                    v-model="selectedAreas"
-                    :items="areas"
-                    label="지역 선택"
-                    multiple
-                    style="width: 250px; display: inline-block; margin-right: 40px; font-size: 12px;"
-                    persistent-hint>
-                </v-select>
+        <v-select
+          v-model="selectedAnimals"
+          :items="animals"
+          label="보호 동물 선택"
+          multiple
+          style="width: 200px; display: inline-block; font-size: 12px;"
+          persistent-hint
+        >
+        </v-select>
+      </div>
 
-                <v-select
-                    v-model="selectedAnimals"
-                    :items="animals"
-                    label="보호 동물 선택"
-                    multiple
-                    style="width: 200px; display: inline-block; font-size: 12px;"
-                    persistent-hint>
-                </v-select>
-            </div>
+      <v-data-table
+        style="margin-top: 100px;"
+        :headers="headers"
+        :items="filteredList()"
+        :items-per-page="10"
+        :search="searchKeyword"
+        loading-text="정보를 불러오는 중입니다."
+        class="elevation-1"
+      >
+        <template v-slot:item="{ item }">
+          <tr @click="toReadPage(item.facilityNo, item.facilityAddr)">
+            <td>{{ item.facilityName }}</td>
+            <td>{{ item.facilityAddr }}</td>
+            <td>{{ item.facilityTel }}</td>
+            <td>{{ item.orgName }}</td>
+            <td>{{ item.saveTrgtAnimal }}</td>
+          </tr>
+        </template>
+      </v-data-table>
 
-            <v-data-table
-                style="margin-top: 100px;"
-                :headers="headers"
-                :items="filteredList()"
-                :items-per-page="10"
-                :search="searchKeyword"
-                loading-text="정보를 불러오는 중입니다."
-                class="elevation-1">
-
-                <template v-slot:item="{ item }"> 
-
-                        <tr @click="toReadPage(item.facilityNo, item.facilityAddr)">
-
-                            <td>{{ item.facilityName }}</td>
-                            <td>{{ item.facilityAddr }}</td>
-                            <td>{{ item.facilityTel }}</td>
-                            <td>{{ item.orgName }}</td>
-                            <td>{{ item.saveTrgtAnimal }}</td>
-                        </tr>
-                    </template>
-
-            </v-data-table>
-            
-            <label style="float: right; margin-top: 30px;">
-                검색 키워드
-                <input v-model="searchKeyword" style="width: 200px;"/>
-            </label>
-
-        </v-container>
-
-    </div>
+      <label style="float: right; margin-top: 30px;">
+        검색 키워드
+        <input v-model="searchKeyword" style="width: 200px;" />
+      </label>
+    </v-container>
+  </div>
 </template>
 
 <script>
-
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
 export default {
-    name: 'FacilityListPage',
-    data() {
-        return {
-            headers:
-                [
-                { text: '보호소명', value: 'facilityName' },
-                { text: '주소', value: 'facilityAddr' },
-                { text: '번호', value: 'facilityTel' },
-                { text: '지역', value: 'orgName' },
-                { text: '보호 동물', value: 'saveTrgtAnimal' }
-                ],
-            
-            searchKeyword: '',
+  name: "FacilityListPage",
+  data() {
+    return {
+      headers: [
+        { text: "보호소명", value: "facilityName" },
+        { text: "주소", value: "facilityAddr" },
+        { text: "번호", value: "facilityTel" },
+        { text: "지역", value: "orgName" },
+        { text: "보호 동물", value: "saveTrgtAnimal" },
+      ],
 
-            areas: [ '서울', '경기', '인천', '강원', '충북', '충남', '충북', '전북', '전남', '경북', '경남', '부산', '대구', '제주' ],
-            selectedAreas: [],
+      searchKeyword: "",
 
-            animals: [ '개', '고양이', '기타' ],
-            selectedAnimals: []
+      areas: [
+        "서울",
+        "경기",
+        "인천",
+        "강원",
+        "충북",
+        "충남",
+        "충북",
+        "전북",
+        "전남",
+        "경북",
+        "경남",
+        "부산",
+        "대구",
+        "제주",
+      ],
+      selectedAreas: [],
 
+      animals: ["개", "고양이", "기타"],
+      selectedAnimals: [],
+    };
+  },
+  methods: {
+    filteredList() {
+      if (this.selectedAreas == "" && this.selectedAnimals == "")
+        return this.$store.state.facilityList;
+
+      var fl = [];
+
+      for (var i = 0; i < this.$store.state.facilityList.length; i++) {
+        if (this.selectedAreas.length > 0 && this.selectedAnimals.length > 0) {
+          var selectedAreasLen = this.selectedAreas.length;
+          var selectedAnimalsLen = this.selectedAnimals.length;
+
+          const length =
+            selectedAreasLen > selectedAnimalsLen
+              ? selectedAreasLen
+              : selectedAnimalsLen;
+
+          for (var j = 0; j < length; j++) {
+            if (
+              this.$store.state.facilityList[i].facilityAddr.includes(
+                this.selectedAreas[j]
+              ) &&
+              this.$store.state.facilityList[i].saveTrgtAnimal.includes(
+                this.selectedAnimals[j]
+              )
+            ) {
+              fl.push(this.$store.state.facilityList[i]);
             }
-    },
-    methods: {
-        filteredList() {
-
-            if(this.selectedAreas == '' && this.selectedAnimals == '') return this.$store.state.facilityList
-
-            var fl = []
-
-            for(var i=0; i<this.$store.state.facilityList.length; i++) {
-
-                if(this.selectedAreas.length > 0 && this.selectedAnimals.length > 0) {
-
-                    var selectedAreasLen = this.selectedAreas.length
-                    var selectedAnimalsLen = this.selectedAnimals.length
-
-                    const length = selectedAreasLen > selectedAnimalsLen ? selectedAreasLen : selectedAnimalsLen
-                
-                    for(var j=0; j<length; j++) {
-
-                        if(this.$store.state.facilityList[i].facilityAddr.includes(this.selectedAreas[j]) && 
-                        this.$store.state.facilityList[i].saveTrgtAnimal.includes(this.selectedAnimals[j])) {
-
-                            fl.push(this.$store.state.facilityList[i])
-                        }
-                    }
-                }
-
-                else if(this.selectedAreas.length > 0) {
-
-                    for(var k=0; k<this.selectedAreas.length; k++) {
-
-                        if(this.$store.state.facilityList[i].facilityAddr.includes(this.selectedAreas[k])) {
-
-                            fl.push(this.$store.state.facilityList[i])
-                        }
-                    }
-                }
-
-                else if(this.selectedAnimals.length > 0) {
-
-                    for(var l=0; l<this.selectedAnimals.length; l++) {
-
-                        if(this.$store.state.facilityList[i].saveTrgtAnimal.includes(this.selectedAnimals[l])) {
-
-                            fl.push(this.$store.state.facilityList[i])
-                        }
-                    }
-                }
+          }
+        } else if (this.selectedAreas.length > 0) {
+          for (var k = 0; k < this.selectedAreas.length; k++) {
+            if (
+              this.$store.state.facilityList[i].facilityAddr.includes(
+                this.selectedAreas[k]
+              )
+            ) {
+              fl.push(this.$store.state.facilityList[i]);
             }
-
-            return fl
-        },
-        toReadPage(facilityNo, facilityAddr) {
-
-            this.$router.push({
-                name: 'FacilityReadPage',
-                params: { facilityNo: facilityNo, facilityAddr: facilityAddr }
-            })
+          }
+        } else if (this.selectedAnimals.length > 0) {
+          for (var l = 0; l < this.selectedAnimals.length; l++) {
+            if (
+              this.$store.state.facilityList[i].saveTrgtAnimal.includes(
+                this.selectedAnimals[l]
+              )
+            ) {
+              fl.push(this.$store.state.facilityList[i]);
+            }
+          }
         }
+      }
+
+      return fl;
     },
-    mounted() {
-        this.$store.dispatch("fetchFacilityList")
+    toReadPage(facilityNo, facilityAddr) {
+      this.$router.push({
+        name: "FacilityReadPage",
+        params: { facilityNo: facilityNo, facilityAddr: facilityAddr },
+      });
     },
-    computed: {
-        ...mapState(['facilityList'])
-    }
-    
-}
+  },
+  mounted() {
+    this.$store.dispatch("fetchFacilityList");
+  },
+  computed: {
+    ...mapState(["facilityList"]),
+  },
+};
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Gowun+Batang:wght@700&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Gowun+Batang:wght@700&display=swap");
 
 tr:hover {
-    font-family: 'Gowun Batang', serif;
-    color: #42b8d4;
+  font-family: "Gowun Batang", serif;
+  color: #42b8d4;
 }
-
 </style>
--- petto-frontend/src/views/map/KakaoMap.vue
+++ petto-frontend/src/views/map/KakaoMap.vue
@@ -8,65 +8,20 @@
 </template>
 
 <script>
+import { mapActions, mapState } from "vuex";
 export default {
   name: "KakaoMap",
   data() {
     return {
       map: null,
-      facility: [
-        {
-          title: "원주유기동물보호관리센터",
-          addr: "강원도 원주시 호저면 칠봉로 110-6",
-        },
-        {
-          title: "화천군농업기술센터",
-          addr: "강원도 화천군 상서면 영서로 6387-9  화천군농업기술센터",
-        },
-        {
-          title: "홍익동물종합병원",
-          addr: "서울특별시 마포구 독막로 45 (합정동)",
-        },
-        {
-          title: "화원연합동물병원",
-          addr: "대구광역시 달성군 화원읍 사문진로 447",
-        },
-        {
-          title: "최종주동물병원",
-          addr: "충청북도 영동군 영동읍 계산로 54",
-        },
-        {
-          title: "현풍동물병원",
-          addr: "대구광역시 달성군 유가읍 테크노공원로 51 (유가읍)",
-        },
-        {
-          title: "21세기동물병원",
-          addr: "서울특별시 용산구 보광동 259-1",
-        },
-        {
-          title: "C.T종합동물병원",
-          addr:
-            "서울특별시 마포구 만리재로 74 (신공덕동, 신공덕2차삼성래미안) 삼성 래미안상가 117호",
-        },
-        {
-          title: "강동구청 반려동물팀",
-          addr: "서울특별시 강동구 성내로 25 (성내동, 강동구청) 강동구청",
-        },
-        {
-          title: "강릉시동물사랑센터",
-          addr: "강원도 강릉시 성산면 내맬길 172 ",
-        },
-        {
-          title: "강현림동물병원",
-          addr: "서울특별시 양천구 등촌로 160 (목동) 1층",
-        },
-        {
-          title: "경동동물병원",
-          addr: "인천광역시 중구 개항로 68 (경동)",
-        },
-      ],
+
       markers: [],
     };
   },
+  computed: {
+    ...mapState(["animals"]),
+  },
+
   mounted() {
     if (window.kakao && window.kakao.maps) {
       this.initMap();
@@ -78,8 +33,11 @@ export default {
         "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=915cffed372954b7b44804ed422b9cf0&libraries=services,clusterer,drawing";
       document.head.appendChild(script);
     }
+    this.fetchAnimalList();
   },
   methods: {
+    ...mapActions(["fetchAnimalList"]),
+
     initMap() {
       var mapContainer = document.getElementById("map"), // 지도를 표시할 div
         mapOption = {
@@ -93,27 +51,33 @@ export default {
       var clusterer = new kakao.maps.MarkerClusterer({
         map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
         averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-        minLevel: 5, // 클러스터 할 최소 지도 레벨
+        minLevel: 10, // 클러스터 할 최소 지도 레벨
       });
 
       var markers = [];
-      var facility = this.facility;
 
-      for (var i = 0; i < facility.length; i++) {
+      console.log(this.animals);
+      this.animals.length = 1000;
+      var _self = this;
+
+      for (var i = 0; i < this.animals.length; i++) {
         (function(i) {
           // 주소-좌표 변환 객체를 생성합니다
           var geocoder = new kakao.maps.services.Geocoder();
 
           // 주소로 좌표를 검색합니다
-          geocoder.addressSearch(facility[i].addr, function(result, status) {
+          geocoder.addressSearch(_self.animals[i].careaddr, function(
+            result,
+            status
+          ) {
             // 정상적으로 검색이 완료됐으면
             if (status === kakao.maps.services.Status.OK) {
               var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-              var message =
+              /*var message =
                 "latlng: new kakao.maps.LatLng(" + result[0].y + ", ";
-              message += result[0].x + ")";
+              message += result[0].x + ")";*/
 
-              console.log(message);
+              //console.log(message);
 
               // 결과값으로 받은 위치를 마커로 표시합니다
               var marker = new kakao.maps.Marker({
@@ -125,7 +89,7 @@ export default {
               var infowindow = new kakao.maps.InfoWindow({
                 content:
                   '<div style="width:150px;text-align:center;padding:6px 0;">' +
-                  facility[i].title +
+                  _self.animals[i].carenm +
                   "</div>",
               });
 
