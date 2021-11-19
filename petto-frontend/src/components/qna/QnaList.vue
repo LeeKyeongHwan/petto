9e6a1d3d6a2cbac42016868bc4264d0e98202074
airtrade7 <airtrade7@naver.com>
2021-11-19 15:53:56 +0900

    QnA

--- petto-frontend/src/components/qna/QnaList.vue
+++ petto-frontend/src/components/qna/QnaList.vue
@@ -15,9 +15,8 @@
           </td>
         </tr>
 
-        <tr v-else v-for="qnaboard in qnaboards" :key="qnaboard.qnadNo">
+        <tr v-else v-for="qnaboard in calData" :key="qnaboard.qnadNo">
           <td align="center">{{ qnaboard.qnaNo }}</td>
-          <!-- <td align="center">{{ board.title }}</td> -->
           <td align="left">
             <router-link :to="{ name: 'QnaReadPage', params: { qnaNo: qnaboard.qnaNo.toString() } }">
               {{ qnaboard.title }}
@@ -28,17 +27,40 @@
         </tr>
       </table>
     </v-container>
+     <v-pagination :length= "numofpage" v-model="curpagenum" color="#42b8d4" style="background-color: white; box-shadow: 0px 0px white;"> </v-pagination>
+      
+
   </div>
 </template>
 
 <script>
 export default {
     name: 'QnAList',
+    data () {
+    return {
+      curpagenum : 1,
+      datapage : 10
+    }
+    },
     props: {
         qnaboards: {
             type: Array
         }
     },
+    computed: {
+    startOffset() {
+      return ((this.curpagenum - 1) * this.datapage);
+    },
+    endOffset() {
+      return (this.startOffset + this.datapage);
+    },
+    numofpage() {
+        return Math.ceil(this.qnaboards.length / this.datapage);
+    },
+    calData() {
+      return this.qnaboards.slice(this.startOffset, this.endOffset);
+    } 
+    }
 }
 </script>
 
