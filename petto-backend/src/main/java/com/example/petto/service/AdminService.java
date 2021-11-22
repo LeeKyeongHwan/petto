dd5e31c744f8e168db70090716920c9c41b427df
jdy8739 <jdy8739@naver.com>
2021-11-22 21:44:27 +0900

    ,

--- petto-backend/src/main/java/com/example/petto/service/AdminService.java
+++ petto-backend/src/main/java/com/example/petto/service/AdminService.java
@@ -1,12 +1,14 @@
 package com.example.petto.service;
 import com.example.petto.entity.Member;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface AdminService {
 
     public List<Member> memberList( ) throws Exception;
 
+    @Transactional
     public Member read(Long memberNo) throws Exception;
 
     public Member edit(Long memberNo, Member member) throws Exception;
